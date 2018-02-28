4cc3aa87f4f80f4a1eaef95b67005cf62c0200b5
zhoujin <1391020381@qq.com>
2018-02-28 17:38:02 +0800

    puppeteer-deep

--- test.js
+++ test.js
@@ -1,14 +1,12 @@
 const puppeteer = require('puppeteer')
 
-// const tempPath = __dirname + '/static/image'
-
 async function getPic (url) {
   const browser = await puppeteer.launch({headless: false})
   const page = await browser.newPage()
-  await page.goto(url)
-  await page.setViewport({width: 1000, height: 500})
-  await page.screenshot({path: './static/img/bing.png', fullPage: true, type: 'png'})
+  await page.goto(url, {waitUntil: 'networkidle0'})
+  // await page.setViewport({width: 100, height: 500})
+  await page.screenshot({path: './data/puppeteer/puppeteer.png', fullPage: false, type: 'png'})
   await browser.close()
 }
 
-getPic('https://www.bing.com/')
+getPic('https://github.com/GoogleChrome/puppeteer/blob/master/docs/api.md#puppeteerlaunchoptions')
